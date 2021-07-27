ffc46e8b2eb81da019cd708c3ac3e1463119eb42
Michael MacAskill <michael.macaskill@nzbri.org>
2021-07-27 17:34:14 +1200

    2021-07-27 Add censoring date - the censoring date for survival analsyes is now available as a package constant, similar to the study census date

--- R/misc.R
+++ R/misc.R
@@ -3,3 +3,8 @@
 # the census date of the New Zealand MS Prevalence study:
 #' @export
 census_date = lubridate::ymd('2006-03-07')
+
+# the censoring date for survival analyses, set to 15 years after the census
+# date:
+#' @export
+censoring_date = lubridate::ymd('2021-03-07')
